093e22266b33254baf427f855effd2d7942a7875
thanhthao1401 <nttthao18@clc.fitus.edu.vn>
2022-08-13 00:29:22 +0700

    Convert to Chang config

--- quan-ly-nhan-vien/src/core/Controllers.php
+++ quan-ly-nhan-vien/src/core/Controllers.php
@@ -1,7 +1,7 @@
 <?php
     class Controllers{
-        public $host_name = "http://localhost:8888/udpt-doanthuchanh/quan-ly-nhan-vien";
-        #public $host_name = "http://localhost/quan-ly-nhan-vien";
+        #public $host_name = "http://localhost:8888/udpt-doanthuchanh/quan-ly-nhan-vien";
+        public $host_name = "http://localhost/quan-ly-nhan-vien";
         public $api_uc002 = "http://localhost/API/api-quanlynhanvien-uc2/api_uc002";
         public $api_uc010 = "";
         public $api_uc013temp = "";
